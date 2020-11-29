31c9ba95ed6374ee245d4575de453634717c8f96
unknown <992938550845@yandex.ru>
2020-11-29 12:26:56 +0500

    add

---
+++ src/containers/Layout/Layout.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Header from "../Header/Header";
+
+function Layout(props) {
+    return (
+        <div className="wrapper">
+            <Header />
+        </div>
+    );
+}
+
+export default Layout;

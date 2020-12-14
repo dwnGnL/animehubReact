7e6f56ce7488a2ea0d988754c20e5e37b67606b9
DenzelHawking <denzelhawking@gmail.com>
2020-12-14 16:41:18 +0500

    feat(infinite slider): infinite slider added

---
+++ src/components/Slider/Slider.jsx
@@ -0,0 +1,71 @@
+import React, { useState } from 'react';
+import './Slider.css'
+
+function Slider() {
+  const [direction, setDirection] = useState('');
+  const [images, setImages] = useState([9, 0, 1]);
+  const sliderImages = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+  let letChangeDirection = true;
+
+  function toLeftSlide() {
+    if (!letChangeDirection) return;
+    letChangeDirection = false;
+
+    setDirection('left-direction');
+
+    setTimeout(() => {
+      letChangeDirection = true;
+      getCurrentSlide('left');
+      setDirection('');
+    }, 700);
+  }
+
+  function toRightSlide() {
+    if (!letChangeDirection) return;
+    letChangeDirection = false;
+
+    setDirection('right-direction');
+
+    setTimeout(() => {
+      letChangeDirection = true;
+      getCurrentSlide('right');
+      setDirection('');
+    }, 700);
+  }
+
+
+  function getCurrentSlide(choosedDirection) {
+    let currentSlide = images[1];
+    let prevSlide;
+    let nextSlide;
+
+    if (choosedDirection === 'right') {
+      currentSlide >= sliderImages.length - 1 ? currentSlide = 0 : currentSlide += 1;
+    }
+
+    if (choosedDirection === 'left') {
+      currentSlide <= 0 ? currentSlide = sliderImages.length - 1 : currentSlide -= 1;
+    }
+
+    currentSlide <= 0 ? prevSlide = sliderImages.length - 1 : prevSlide = currentSlide - 1;
+    currentSlide >= sliderImages.length - 1 ? nextSlide = 0 : nextSlide = currentSlide + 1;
+
+    setImages([sliderImages[prevSlide], sliderImages[currentSlide], sliderImages[nextSlide]]);
+  }
+
+  return (
+    <div className="slider">
+      <div className="slider__button to-left" onClick={toLeftSlide}>left</div>
+      <div className="slider__button to-right" onClick={toRightSlide}>right</div>
+
+      <div className={`slider__container ${direction}`}>
+        <div className="slider__item slider__item-0">{images[0]}</div>
+        <div className="slider__item slider__item-1">{images[1]}</div>
+        <div className="slider__item slider__item-2">{images[2]}</div>
+      </div>
+    </div>
+  )
+}
+
+export default Slider;
--- src/containers/Layout/Layout.jsx
+++ src/containers/Layout/Layout.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Header from "../Header/Header";
+import Slider from '../../components/Slider/Slider'
 
 function Layout(props) {
     return (
         <div className="wrapper">
             <Header />
+            <Slider />
         </div>
     );
 }

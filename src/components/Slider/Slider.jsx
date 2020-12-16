fa5978104834a6e632223ef789a21578a156fd8c
DenzelHawking <denzelhawking@gmail.com>
2020-12-16 10:37:55 +0500

    feat(background slider): background slider added

--- src/components/Slider/Slider.jsx
+++ src/components/Slider/Slider.jsx
@@ -3,9 +3,13 @@ import './Slider.css'
 
 
 function Slider() {
+  const delay = 700;
   const [direction, setDirection] = useState('');
   const [images, setImages] = useState(['', '', '']);
   const [sliderImages, setSliderImages] = useState([]);
+  const [sliderBackgroundPart, setSliderBackgroundPart] = useState(0);
+  const [sliderBackgroundClass, setSliderBackgroundClass] = useState('part_0');
+  const [changeDirection, setChangeDirection] = useState(true);
 
   useEffect(() => {
     fetch('https://swapi.dev/api/people/1')
@@ -17,32 +21,32 @@ function Slider() {
   }, [])
 
 
-  let letChangeDirection = true;
-
   function toLeftSlide() {
-    if (!letChangeDirection) return;
-    letChangeDirection = false;
+    if (!changeDirection) return;
+    setChangeDirection(false);
 
     setDirection('left-direction');
+    moveBackgroundSlider('left');
 
     setTimeout(() => {
-      letChangeDirection = true;
+      setChangeDirection(true)
       getCurrentSlide('left');
       setDirection('');
-    }, 700);
+    }, delay);
   }
 
   function toRightSlide() {
-    if (!letChangeDirection) return;
-    letChangeDirection = false;
+    if (!changeDirection) return;
+    setChangeDirection(false);
 
     setDirection('right-direction');
+    moveBackgroundSlider('right');
 
     setTimeout(() => {
-      letChangeDirection = true;
+      setChangeDirection(true)
       getCurrentSlide('right');
       setDirection('');
-    }, 700);
+    }, delay);
   }
 
 
@@ -53,17 +57,51 @@ function Slider() {
 
     if (choosedDirection === 'right') currentSlide >= sliderImages.length - 1 ? currentSlide = 0 : currentSlide += 1;
     if (choosedDirection === 'left') currentSlide <= 0 ? currentSlide = sliderImages.length - 1 : currentSlide -= 1;
-    
+
     currentSlide <= 0 ? prevSlide = sliderImages.length - 1 : prevSlide = currentSlide - 1;
     currentSlide >= sliderImages.length - 1 ? nextSlide = 0 : nextSlide = currentSlide + 1;
 
     setImages([sliderImages[prevSlide], sliderImages[currentSlide], sliderImages[nextSlide]]);
   }
 
+  function moveBackgroundSlider(choosedDirection) {
+    let movingPart = sliderBackgroundPart;
+
+    if (choosedDirection === 'right') {
+      movingPart++;
+
+      if (movingPart >= 3) {
+        setTimeout(() => {
+          movingPart = 0
+          setSliderBackgroundClass(`part_${0}`)
+          setSliderBackgroundPart(0)
+        }, delay);
+      }
+    }
+
+    if (choosedDirection === 'left') {
+      movingPart--;
+
+      if (movingPart <= -3) {
+        setTimeout(() => {
+          movingPart = 0
+          setSliderBackgroundClass(`part_${0}`)
+          setSliderBackgroundPart(0)
+        }, delay);
+      }
+    }
+
+    setSliderBackgroundClass(`part_${movingPart} animated`)
+    setSliderBackgroundPart(movingPart)
+  }
+  
+
   return (
     <div className="slider">
       <div className="slider__button to-left" onClick={toLeftSlide}>left</div>
       <div className="slider__button to-right" onClick={toRightSlide}>right</div>
+
+      <div className={`slider__background ${direction} ${sliderBackgroundClass}`}></div>
 
       <div className={`slider__container ${direction}`}>
         <div className="slider__item slider__item-0">{images[0]}</div>

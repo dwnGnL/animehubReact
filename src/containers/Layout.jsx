06a0d478503b17feea4d0c14a8b72b286398db75
DenzelHawking <ankhid98@gmail.com>
2021-08-11 09:26:43 +0500

    devices added

--- src/containers/Layout.jsx
+++ src/containers/Layout.jsx
@@ -1,24 +1,38 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import Slider from "../components/slider/Slider";
 import Header from "../components/header/Header";
 
-function Layout(props) {
-    const [images, setImages] = useState([]);
-    
-    useEffect(() => {
-        fetch('https://swapi.dev/api/people/1')
-        .then(response => response.json())
-        .then(request => {
-          setImages(request.films);
-        });
-    }, []);
+function Layout() {
+  const wrapper = useRef();
+  const [images, setImages] = useState([]);
+  const [device, setDevice] = useState('');
 
-    return (
-        <div className="wrapper">
-            <Header />
-            {images.length && <Slider allImages={images} />}
-        </div>
-    );
+  useEffect(() => {
+    let wrapperWidth = wrapper.current.offsetWidth;
+
+    if (wrapperWidth <= 850 && wrapperWidth > 500) {
+      setDevice('tablet');
+    } else if (wrapperWidth <= 500) {
+      setDevice('mobile');
+    } else {
+      setDevice('desktop');
+    };
+  }, [device]);
+
+  useEffect(() => {
+    fetch("https://swapi.dev/api/people/1")
+      .then((response) => response.json())
+      .then((request) => {
+        setImages(request.films);
+      });
+  }, []);
+
+  return (
+    <div className="wrapper" ref={wrapper} data-device={device}>
+      <Header device={device} />
+      {images.length !== 0 ? <Slider allImages={images} /> : null}
+    </div>
+  );
 }
 
 export default Layout;

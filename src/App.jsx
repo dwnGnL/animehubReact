edde7f12b8b9b02b514e5df04e9df4ec8085e2c7
DenzelHawking <ankhid98@gmail.com>
2021-08-12 17:29:49 +0500

    containers component moved to App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,30 @@
+import React, { useState, useEffect, useRef } from "react";
+import Header from "./containers/header/Header";
+import Content from "./containers/content/Content";
 import "./App.css";
-import Layout from "./containers/Layout";
 
 function App() {
-  return <Layout />;
+  const wrapper = useRef();
+  const [device, setDevice] = useState('');
+
+  useEffect(() => {
+    let wrapperWidth = wrapper.current.offsetWidth;
+
+    if (wrapperWidth <= 850 && wrapperWidth > 568) {
+      setDevice('tablet');
+    } else if (wrapperWidth <= 568) {
+      setDevice('mobile');
+    } else {
+      setDevice('desktop');
+    };
+  }, [device]);
+
+  return (
+    <div className="wrapper" ref={wrapper} data-device={device}>
+      <Header device={device} />
+      <Content />
+    </div>
+  );
 }
 
 export default App;

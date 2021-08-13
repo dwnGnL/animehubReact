094af9c323953aa074f8f84256b575d394d6e68f
DenzelHawking <ankhid98@gmail.com>
2021-08-13 09:09:49 +0500

    Logo component renamed

--- src/components/common/Logo.jsx
+++ src/components/common/Logo.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 import LogoImg from "../../assets/logo.png";
 
-function AnimehubLogo() {
+function Logo() {
   return <div className="logo" style={{background: `url(${LogoImg}) center no-repeat`, backgroundSize: 'contain'}} />
 }
 
-export default AnimehubLogo;
+export default Logo;

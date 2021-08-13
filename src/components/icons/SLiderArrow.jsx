e22422035247ad6be486c8a24d294c1536fc1b88
DenzelHawking <ankhid98@gmail.com>
2021-08-13 09:07:32 +0500

    icon components added

---
+++ src/components/icons/SLiderArrow.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function SLiderArrow() {
+  return (
+    <svg width="80%" height="80%" viewBox="0 0 24 24">
+      <path d="M10.024 4h6.015l7.961 8-7.961 8h-6.015l7.961-8-7.961-8zm-10.024 16h6.015l7.961-8-7.961-8h-6.015l7.961 8-7.961 8z" />
+    </svg>
+  );
+}

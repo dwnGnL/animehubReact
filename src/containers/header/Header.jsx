df7afd16b984d2b259a5052ea307f75a3945fe76
DenzelHawking <ankhid98@gmail.com>
2021-08-17 10:17:35 +0500

    improvement(header) header separeded

--- src/containers/header/Header.jsx
+++ src/containers/header/Header.jsx
@@ -1,25 +1,30 @@
 import React, { useState, useContext } from "react";
-import { HeaderContext, MenuContext } from '../../Context';
+import { HeaderContext } from '../../Context';
+import SubMenu from './SubMenu';
 import Logo from "../../components/common/Logo";
 import Button from "../../components/common/Button";
-import { CloseBtn } from "../../components/common/Icons";
+import { navLinks } from './HeaderInner';
 import "./header.css";
 
 
 function Header(props) {
   const [menuOpened, setMenuOpened] = useState('');
-
+  const [subMenuOpened, setSubMenuOpened] = useState('');
 
   function menuHandler(isOpened) {
     isOpened ? setMenuOpened(' menu-opened') : setMenuOpened(' menu-closed');
   }
 
+  function subMenuHandler(isOpened) {
+    isOpened  ? setSubMenuOpened(' sub-menu-opened') : setSubMenuOpened(' sub-menu-closed');
+  }
+
   return (
-    <HeaderContext.Provider value={menuHandler}>
+    <HeaderContext.Provider value={{menuHandler: menuHandler, subMenuHandler: subMenuHandler}}>
       <header className="header">
         <div className="left_side">
           <Logo />
-          {props.device === 'desktop' ? <Menu activeMenuClass={menuOpened} device={props.device} /> : null}
+          {props.device === 'desktop' ? <Menu activeMenuClass={menuOpened} activeSubMenuClass={subMenuOpened} device={props.device} /> : null}
         </div>
         
         <div className="right_side">
@@ -27,13 +32,13 @@ function Header(props) {
           {props.device === 'tablet' || props.device === 'mobile'  ? <MenuHamburger activeMenuClass={menuOpened} /> : null}
         </div>
       </header>
-      {props.device === 'tablet' || props.device === 'mobile'  ? <Menu activeMenuClass={menuOpened} device={props.device} /> : null}
+      {props.device === 'tablet' || props.device === 'mobile'  ? <Menu activeMenuClass={menuOpened} activeSubMenuClass={subMenuOpened} device={props.device} /> : null}
     </HeaderContext.Provider>
   );
 }
 
 function MenuHamburger(props) {
-  const menuHandler = useContext(HeaderContext);
+  const {menuHandler} = useContext(HeaderContext);
 
   return (
     <div className={'hamburger_menu' + props.activeMenuClass} onClick={() => menuHandler(true)}>
@@ -45,183 +50,26 @@ function MenuHamburger(props) {
 }
 
 function Menu(props) {
-  const [activeMenuItemClass, setActiveMenuItemClass] = useState('');
-  const menuHandler = useContext(HeaderContext);
+  const {menuHandler, subMenuHandler} = useContext(HeaderContext);
 
-  const navLinks = [
-    {
-      title: "Дорамы",
-      id: 5,
-    },
-    {
-      title: "Онгионги",
-      id: 6,
-    },
-    {
-      title: "Магазин",
-      id: 7,
-    },
-    {
-      title: "Топ аниме",
-      id: 8,
-    },
-  ];
-
-  function subMenuHandler(isOpened) {
-    isOpened  ? setActiveMenuItemClass(' sub-menu-opened') : setActiveMenuItemClass(' sub-menu-closed');
+  function closeAll() {
+    menuHandler(false);
+    if (props.activeSubMenuClass !== '') subMenuHandler(false);
   }
 
   return (
-    <MenuContext.Provider value={subMenuHandler}>
-      <div className={'menu-wrapper' + props.activeMenuClass + activeMenuItemClass} onClick={() => menuHandler(false)}>
-        <nav className='menu' onClick={(event) => event.stopPropagation()}>
-          <div className={'menu-item'} onMouseEnter={() => subMenuHandler(true)} onMouseLeave={() => props.device === 'desktop' ? subMenuHandler(false) : null}>
-            <div className="main-item">Аниме</div>
-            {props.device === 'desktop' ? <SubMenu device={props.device} /> : null}
-          </div>
-          {navLinks.map((item) => <div className={'menu-item'} onClick={() => menuHandler(false)} key={item.id}>{item.title}</div>)}
-        </nav>
+    <div className={'menu-wrapper' + props.activeMenuClass + props.activeSubMenuClass} onClick={closeAll}>
+      <nav className='menu' onClick={(event) => event.stopPropagation()}>
+        <div className={'menu-item'} onMouseEnter={() => subMenuHandler(true)} onMouseLeave={() => props.device === 'desktop' ? subMenuHandler(false) : null}>
+          <div className="main-item">Аниме</div>
+          {props.device === 'desktop' ? <SubMenu device={props.device} /> : null}
+        </div>
+        {navLinks.map((item) => <div className={'menu-item'} onClick={() => menuHandler(false)} key={item.id}>{item.title}</div>)}
+      </nav>
 
-        {props.device === 'mobile' || props.device === 'tablet' ? <SubMenu device={props.device} /> : null}
-      </div>
-    </MenuContext.Provider>
+      {props.device === 'mobile' || props.device === 'tablet' ? <SubMenu device={props.device} /> : null}
+    </div>
   )
-}
-
-function SubMenu(props) {
-  const subMenuHandler = useContext(MenuContext);
-
-  return (
-    <div className="sub-menu" onClick={(event) => event.stopPropagation()}>
-      {props.device === 'mobile' || props.device === 'tablet' ? <CloseBtn closeFn={() => subMenuHandler(false)} width='8' color='#000' /> : null}
-      <LeftSubMenu />
-      <RightSubMenu />
-    </div>
-  );
-}
-
-function LeftSubMenu() {
-  const listTypes = [
-    {
-      title: "ТВ",
-      id: 1,
-    },
-    {
-      title: "OVA",
-      id: 2,
-    },
-    {
-      title: "Фильм",
-      id: 3,
-    },
-    {
-      title: "AMV",
-      id: 4,
-    },
-  ];
-
-  const listYears = [
-    2021, 2020, 2019, 2018, 2017, 2016, 2015, 2014, 2013, 2012, 2011, 2010,
-    2009, 2008, 2007, 2006, 2005, 2004, 2003, 2002, 2001, 2000, 1999, 1998,
-    1997, 1996, 1995, 1994, 1993, 1992, 1991, 1990, 1989, 1988, 1987, 1986,
-    1985, 1984
-  ];
-
-  return (
-    <div className="left">
-      <TypeList list={listTypes} />
-      <YearList list={listYears} />
-    </div>
-  );
-}
-
-function RightSubMenu() {
-  const listGenre = [
-    "детектив",
-    "жестокие сцены",
-    "комедия",
-    "мистика",
-    "наше время",
-    "повседневность",
-    "боевые искусства",
-    "вампиры",
-    "война",
-    "Гарем",
-    "детектив",
-    "драма",
-    "исекай",
-    "история",
-    "киберпанк",
-    "комедия",
-    "махо-сёдзё",
-    "меха",
-    "мистерия",
-    "мистика",
-    "музыкальный",
-    "Онгоинг",
-    "пародия",
-    "повседневность",
-    "Приключения",
-    "романтика",
-    "самурайский боевик",
-    "Сёдзё",
-    "сёдзё-ай",
-    "Сёнэн",
-    "сёнэн-ай",
-    "спорт",
-    "триллер",
-    "ужасы",
-    "фантастика",
-    "Фэнтези",
-    "школа",
-    "этти"
-  ];
-
-  const separateListGenre = [];
-  separateListGenre.push(listGenre.slice(0, listGenre.length / 2));
-  separateListGenre.push(listGenre.slice(listGenre.length / 2, listGenre.length));
-
-  return (
-    <div className="right">
-      {separateListGenre.map((genre, index) => {
-        return (
-          <nav className="list-nav" key={'submenu-' + index}>
-            <RightSubMenuLinks list={genre} />
-          </nav>
-        );
-      })}
-    </div>
-  );
-}
-
-function TypeList(props) {
-  return (
-    <div className="type_list">
-      <span className="sub-menu-header">По типу</span>
-      <nav>
-        {props.list.map((item) => <a href="./" key={item.id} className="sub-menu-item">{item.title}</a>)}
-      </nav>
-    </div>
-  );
-}
-
-function YearList(props) {
-  return (
-    <div className="year_list">
-      <span className="sub-menu-header">По годам</span>
-      <nav>
-        {props.list.map((item, index) => <a href="./" key={index} className="sub-menu-item">{item}</a>)}
-      </nav>
-    </div>
-  );
-}
-
-function RightSubMenuLinks(props) {
-  return (
-    <div className="link">
-      {props.list.map((item, index) => <a href="./" key={index} className="list-nav-item">{item}</a>)}
-    </div>
-  );
 }
 
 export default Header;
---
+++ src/containers/header/SubMenu.jsx
@@ -0,0 +1,76 @@
+import React, { useContext } from 'react';
+import { listTypes, listYears, listGenre } from './HeaderInner';
+import { HeaderContext } from '../../Context';
+import { CloseBtn } from '../../components/common/Icons';
+
+function SubMenu(props) {
+  const {subMenuHandler} = useContext(HeaderContext);
+
+  return (
+    <div className="sub-menu" onClick={(event) => event.stopPropagation()}>
+      {props.device === 'mobile' || props.device === 'tablet' ? <CloseBtn closeFn={() => subMenuHandler(false)} width='8' color='#000' /> : null}
+      <LeftSubMenu />
+      <RightSubMenu />
+    </div>
+  );
+}
+
+function LeftSubMenu() {
+  return (
+    <div className="left">
+      <TypeList list={listTypes} />
+      <YearList list={listYears} />
+    </div>
+  );
+}
+
+function RightSubMenu() {
+  const separateListGenre = [];
+  separateListGenre.push(listGenre.slice(0, listGenre.length / 2));
+  separateListGenre.push(listGenre.slice(listGenre.length / 2, listGenre.length));
+
+  return (
+    <div className="right">
+      {separateListGenre.map((genre, index) => {
+        return (
+          <nav className="list-nav" key={'submenu-' + index}>
+            <RightSubMenuLinks list={genre} />
+          </nav>
+        );
+      })}
+    </div>
+  );
+}
+
+function TypeList(props) {
+  return (
+    <div className="type_list">
+      <span className="sub-menu-header">По типу</span>
+      <nav>
+        {props.list.map((item) => <a href="./" key={item.id} className="sub-menu-item">{item.title}</a>)}
+      </nav>
+    </div>
+  );
+}
+
+function YearList(props) {
+  return (
+    <div className="year_list">
+      <span className="sub-menu-header">По годам</span>
+      <nav>
+        {props.list.map((item, index) => <a href="./" key={index} className="sub-menu-item">{item}</a>)}
+      </nav>
+    </div>
+  );
+}
+
+function RightSubMenuLinks(props) {
+  return (
+    <div className="link">
+      {props.list.map((item, index) => <a href="./" key={index} className="list-nav-item">{item}</a>)}
+    </div>
+  );
+}
+
+
+export default SubMenu;

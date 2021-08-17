9e2c3dba05f32bc0655166f54503378714ea529e
DenzelHawking <ankhid98@gmail.com>
2021-08-17 15:47:57 +0500

    slider removed from main tab component

--- src/tabs/Main.jsx
+++ src/tabs/Main.jsx
@@ -1,11 +1,9 @@
 import React from 'react';
-import Slider from '../components/slider/Slider';
 import Input from '../components/common/input/Input';
 
 function Main() {
   return (
     <>
-      <Slider />
       <Input name="search_film" placeholder="Поиск аниме" />
     </>
   );
